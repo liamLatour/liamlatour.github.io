c1392e4d9f12b557450c3a5c8d0c72b22b3099df
Liam LATOUR <liam.latour@etu.emse.fr>
2023-09-23 18:39:14 +0200

    Updated linkedin url

--- components/ContactInfo.vue
+++ components/ContactInfo.vue
@@ -8,7 +8,7 @@
         {{$t('contact.text')}} <a class="font-bold" href="mailto:liam.latour@gmail.com">liam.latour@gmail.com</a>
       </h3>
       <div class="mt-8 text-xl flex gap-8 justify-center">
-        <a href="https://www.linkedin.com/in/liam-latour-937171222" class="p-3">
+        <a href="https://www.linkedin.com/in/liam-latour" class="p-3">
           <font-awesome-icon :icon="['fab', 'linkedin-in']"/>
         </a>
         <a href="https://github.com/liamLatour" class="p-3">
